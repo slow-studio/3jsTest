99ce28bda94d9351ed98ff479809270e4b1cff2d
PiyaRatel <37631796+PiyaRatel@users.noreply.github.com>
2021-07-08 09:48:12 +0530

    Add files via upload

---
+++ 10. buttong in the gltf folder/main.js
@@ -0,0 +1,14 @@
+// Defining backgroung coulour
+function changeColor(color) {
+    document.body.style.background = color;
+}
+
+changeColor('black');
+
+btn.onclick = function() {
+    //do stuff
+    
+    changeColor('yellow');
+    
+
+}
